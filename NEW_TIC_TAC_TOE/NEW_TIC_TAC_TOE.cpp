539f00853fcd2521e5d82b47c7bd0342d0ca3f0a
Shlmp <daniel.kil@uniat.edu.mx>
2023-06-29 10:54:42 -0600

    Added Teachers TicTacToe

--- NEW_TIC_TAC_TOE/NEW_TIC_TAC_TOE.cpp
+++ NEW_TIC_TAC_TOE/NEW_TIC_TAC_TOE.cpp
@@ -1,268 +1,281 @@
+//Tic-Tac-Toe
+//Playing Tic Tac Toe with an AI
 #include <iostream>
 #include <string>
-#include <vector>
 #include <algorithm>
-#include <cstdlib>
-#include <clocale>
+#include <vector>
+#include <locale.h>
 
 using namespace std;
 
-const int NUM_SQUARES = 9;
-const char EMPTY = ' ';
+//Global Constants
 const char X = 'X';
 const char O = 'O';
-const char NO_ONE = 'N';
 const char TIE = 'T';
-const vector<char> board(NUM_SQUARES, EMPTY);
+const char EMPTY = ' ';
+const char NO_ONE = 'N';
 
-void instructions();
-char playerSymbol();
-char opponent(char& player);
-string askYesNo(string question);
+//Prototype Functions
 char winner(const vector<char>& board);
-int askNumber(string question, int high);
-void displayBoard(vector<char>& board);
-int playerMove(const vector<char>& board, char player);
+void instructions();
+void displayBoard(const vector<char>& board);
+char askYesNo(string question);
+char humanSymbol();
+char opponent(char symbol);
+int askNumber(string question, int high, int low = 0);
 bool isLegal(int move, const vector<char>& board);
+int humanMove(const vector<char>& board);
+int computerMove(vector<char> board, char computer);
+void announceWinner(char winner, char human, char computer);
 
-int main()
-{
-    srand(time(NULL));
-    char player = playerSymbol();
-    char computer = opponent(player);
-    instructions();
-    //char turn = X;
-    vector<char> boardCopy = board;
 
-    while (winner(board) == NO_ONE)
-    {
-        cout << "Player - " << player << "    AI - " << computer << endl;
-        if (/*turn == */player)
-        {
-            playerMove(boardCopy, player);
-        }
-        displayBoard(boardCopy);
-    }
+//Function main
+int main() {
+	setlocale(LC_ALL, "spanish");
+
+	const int NUM_SQUARES = 9;
+	int move;
+	vector<char> board(NUM_SQUARES, EMPTY);
+
+	instructions();
+	char player = humanSymbol();
+	char computer = opponent(player);
+	char turn = X;
+
+	//displayBoard(board);
+
+	while (winner(board) == NO_ONE) {
+
+		if (turn == player) {
+			move = humanMove(board);
+			board[move] = player;
+		}
+		else {
+			move = computerMove(board, computer);
+			board[move] = computer;
+		}
+
+		turn = opponent(turn);
+		displayBoard(board);
+	}
+
+	announceWinner(winner(board), player, computer);
+
 }
 
-void instructions()
-{
-    //system("cls");
-    cout << "Welcome to Tic-Tac-Toe\nChoose a space between 0 - 8\n";
-    cout << "0 | 1 | 2\n";
-    cout << "3 | 4 | 5\n";
-    cout << "6 | 7 | 8\n";
-    //for (int i = 0; i < 3; i++)
-    //{
-    //    for (int j = 0; j < 3; j++)
-    //    {
-    //        cout << board[j];
-    //        if (j < 2) cout << " |";
-    //    }
-    //    cout << endl;
-    //}
-}
-
-char playerSymbol()
-{
-    string goFirst = askYesNo("ODD or EVEN, What do you choose?");
-    int min = 1;
-    int max = 6;
-    int die = rand() % (max - min + 1) + 1;
-    if (goFirst == "EVEN")
-    {
-        if (die == 2 || die == 4 || die == 6)
-        {
-            cout << "CONGRATULATIONS!!   You rolled an even number: " << die << endl;
-            return X;
-        }
-        else
-        {
-            cout << "HA LOSER!     You rolled an odd number: " << die << endl;
-            return O;
-        }
-    }
-    else
-    {
-        if (die == 1 || die == 3 || die == 5)
-        {
-            cout << "CONGRATULATIONS!!   You rolled an odd number: " << die << endl;
-            return X;
-        }
-        else
-        {
-            cout << "HA LOSER!     You rolled an even number: " << die << endl;
-            return O;
-        }
-    }
-}
-
-char opponent(char& player)
-{
-    if (player == X)
-    {
-        return O;
-    }
-    else
-        return X;
-}
-
-string askYesNo(string question)
-{
-    string answer;
-
-    do
-    {
-        cout << "\n" << question << endl;
-        cin >> answer;
-        transform(answer.begin(), answer.end(), answer.begin(), ::toupper);
-    } while (answer != "ODD" && answer != "EVEN");
-    //if (answer == 'y')
-    //{
-    //    return X;
-    //}
-    return answer;
-}
-
+//Return the piece winner a tie or noBody
 char winner(const vector<char>& board)
 {
-    //Winning Positions
-    const int WINNING_POS[8][3] = { { 0, 1, 2 }, { 3, 4, 5 }, { 6, 7, 8 },  //Horizontal
-                                    { 0, 3, 6 }, { 1, 4, 7 }, { 2, 5, 8 },  //Vertical
-                                    { 2, 4, 6 }, { 0, 4, 8 } };             //Diagonal
-    const int TOTAL_ROWS = 8;
+	//This are the posibilities to win.
+	const int WINNING_POS[8][3] = { {0, 1, 2},
+									{3, 4, 5},
+									{6, 7, 8},
+									{0, 3, 6},
+									{1, 4, 7},
+									{2, 5, 8},
+									{2, 4, 6},
+									{0, 4, 8} };
 
-    //Return Winner
-    for (int row = 0; row < TOTAL_ROWS; row++)
-    {
-        if ((board[WINNING_POS[row][0]] != EMPTY) &&
-            (board[WINNING_POS[row][0]] == board[WINNING_POS[row][1]]) &&
-            (board[WINNING_POS[row][1]] == board[WINNING_POS[row][2]]))     //Compares first position to second position and if == then check if second is == to third
-        {
-            return board[WINNING_POS[row][0]];
-        }
-    }
+	const int TOTAL_ROWS = 8;
 
-    //Return Tie
-    if (count(board.begin(), board.end(), EMPTY) == 0)  return TIE;
+	//Return the winner
+	for (int row = 0; row < TOTAL_ROWS; row++)
+	{
+		if ((board[WINNING_POS[row][0]] != EMPTY) &&
+			(board[WINNING_POS[row][0]] == board[WINNING_POS[row][1]]) &&
+			(board[WINNING_POS[row][1]] == board[WINNING_POS[row][2]]))
+		{
+			return board[WINNING_POS[row][0]];
+		}
 
-    //Return if there is no winner
-    return NO_ONE;
+	}
+
+	//Return a Tie
+	if (count(board.begin(), board.end(), EMPTY) == 0) {
+		return TIE;
+	}
+
+	//Return that no one is the winner yet
+	return NO_ONE;
 }
 
-int askNumber(string question, int high)
-{
-    string input;
-    bool isValid = false;
-    bool isRangeValid = false;
-    int number = 0;
+void instructions() {
+	cout << "\n¡Bienvenido al juego de Gato!\n";
+	cout << "Donde te enfrentarás a un digno oponente\n";
+	cout << "Debes elegir un número 0 - 8 para definir una posición en el tablero: \n";
+	cout << " 0 | 1 | 2 \n";
+	cout << " 3 | 4 | 5 \n";
+	cout << " 6 | 7 | 8 \n";
 
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
-
-    do
-    {
-        cout << question << " between " << 0 << " & " << high << endl;
-        getline(cin, input);
-
-        for (char c : input)
-        {
-            if (!isdigit(c))
-            {
-                isValid = false;
-                break;
-            }
-            else
-            {
-                isValid = true;
-                break;
-            }
-        }
-
-        if (!isValid)
-        {
-            cout << "Invalid option\n";
-        }
-        else
-        {
-            number = stoi(input);
-            isRangeValid = number <= high && number >= 0;
-        }
-
-        if (!isRangeValid && isValid)
-        {
-            cout << "Invalid option\n";
-        }
-    } while (!isValid || input.empty() || !isRangeValid);
-    return number;
+	cout << "Prepárate la batalla está a punto de comenzar.\n\n";
 }
 
-void displayBoard(vector<char>& board)
-{
-    cout << *(board.begin() + 0) << " | " << *(board.begin() + 1) << " | " << *(board.begin() + 2) << endl
-        << *(board.begin() + 3) << " | " << *(board.begin() + 4) << " | " << *(board.begin() + 5) << endl
-        << *(board.begin() + 6) << " | " << *(board.begin() + 7) << " | " << *(board.begin() + 8) << endl;
+char askYesNo(string question) {
+	char answer;
+	do {
+		cout << question << "(y/n)";
+		cin >> answer;
+	} while (answer != 'y' && answer != 'n');
+	return answer;
 }
 
-int playerMove(const vector<char>& board, char player)
-{
-    int move = askNumber("Choose the space you want to use", (board.size() - 1));
+int askNumber(string question, int high, int low) {
+	int number;
+	do {
+		cout << question << "(" << low << " - " << high << ")";
+		cin >> number;
+	} while (number > high || number < low);
 
-    while (!isLegal(move, board))
-    {
-        cout << "\nSpace already in use\n";
-        move = askNumber("Choose the space you want to use", (board.size() - 1));
-    }
-
-    return move;
+	return number;
 }
 
-inline bool isLegal(int move, const vector<char>& board)
-{
-    return (board[move] == EMPTY);
+
+char humanSymbol() {
+	char go_first = askYesNo("¿Quieres empezar?");
+
+	if (go_first == 'y')
+	{
+		cout << "Buena elección, la necesitarás\n";
+		return X;
+	}
+	else {
+		cout << "Admiro tu valentía, yo empezaré entonces\n";
+		return O;
+	}
 }
 
-int computerMove(vector<char> board, char computer)
-{
-    int move = 0;
-    bool foundSpace = false;
+char opponent(char symbol) {
+	if (symbol == X) {
+		return O;
+	}
+	else
+	{
+		return X;
+	}
+}
 
-    while (!foundSpace && move < board.size())
-    {
-        if (isLegal(move, board))
-        {
-            board[move] = computer;
-            foundSpace = winner(board) == computer;
-        }
 
-        if (!foundSpace)
-        {
-            move++;
-        }
-    }
+void displayBoard(const vector<char>& board) {
+	cout << "\n\t" << board[0] << " | " << board[1] << " | " << board[2];
+	cout << "\n\t" << board[3] << " | " << board[4] << " | " << board[5];
+	cout << "\n\t" << board[6] << " | " << board[7] << " | " << board[8];
+	cout << "\n";
+}
 
-    if (!foundSpace)
-    {
-        move = 0;
-        char human = opponent(computer);
-        while (!foundSpace && move < board.size())
-        {
-            if (isLegal(move, board))
-            {
-                board[move] = human;
-                foundSpace = winner(board) == human;
-            }
 
-            if (!foundSpace)
-            {
-                move++;
-            }
-        }
-    }
 
-    if (!foundSpace)
-    {
-        move = 0;
-        unsigned int i = 0;
-        //const int BEST_MOVE = { 4, 0, 2, 6, 8, 1, 3, 5, 7 };
-    }
+
+inline bool isLegal(int move, const vector<char>& board) {
+
+	bool isLegal = false;
+
+
+	if (board[move] == EMPTY)
+	{
+		isLegal = true;
+	}
+
+	return isLegal;
+
+
+	//return (board[move] == EMPTY);
+}
+
+
+int humanMove(const vector<char>& board) {
+	int move = askNumber("¿Elige una posición del tablero?", (board.size() - 1));
+
+	while (!isLegal(move, board)) {
+		cout << "\nEsa posición ya está ocupada humano tonto.\n";
+		move = askNumber("¿Elige una posición del tablero?", (board.size() - 1));
+	}
+
+	cout << "Bien..\n";
+	return move;
+}
+
+
+int computerMove(vector<char> board, char computer) {
+	int move = 0;
+	bool foundSpace = false;
+
+
+	while (!foundSpace && move < board.size()) {
+
+		if (isLegal(move, board)) {
+
+
+			board[move] = computer;
+
+			foundSpace = winner(board) == computer;
+
+			if (winner(board) == computer) {
+				foundSpace = true;
+			}
+
+			board[move] = EMPTY;
+		}
+
+		if (!foundSpace) {
+			++move;
+		}
+	}
+
+
+	if (!foundSpace) {
+		move = 0;
+		char human = opponent(computer);
+		while (!foundSpace && move < board.size()) {
+
+			if (isLegal(move, board)) {
+
+
+				board[move] = human;
+
+				foundSpace = winner(board) == human;
+
+				board[move] = EMPTY;
+			}
+
+			if (!foundSpace) {
+				++move;
+			}
+		}
+
+	}
+
+
+	if (!foundSpace) {
+		move = 0;
+		unsigned int i = 0;
+		const int BEST_MOVE[] = { 4, 0, 2, 6, 8, 1, 3, 5, 7 };
+
+		while (!foundSpace && i < board.size()) {
+
+			move = BEST_MOVE[i];
+			if (isLegal(move, board)) {
+				foundSpace = true;
+			}
+
+			++i;
+		}
+	}
+
+	cout << "\nUsaré la posición " << move << "." << endl;
+	return move;
+}
+
+void announceWinner(char winner, char human, char computer) {
+	if (winner == computer) {
+		cout << "\nEl ganador es: " << winner << endl;
+		cout << "\n Soy el ganador!! Probando una vez más que las computadoras somos mejores que ustedes en todo sentido.\n";
+	}
+	else if (winner == human) {
+		cout << "\nEl ganador es: " << winner << endl;
+		cout << "\nNo es posible!! Hiciste trampa humano!!";
+	}
+	else {
+		cout << "\n ES UN EMPATE\n";
+		cout << "\nQue suerte tienes, celébralo es lo mejor que lograrás.\n";
+	}
 }
